deccd4aafc89dd7c9e5967fe58370ca216e6878a
Borislav Dukov <borislav.dukov@icloud.com>
2023-08-21 20:14:35 +0300

    Create App

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsModule } from './products/products.module';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ProductsModule,
+    CoreModule
+  ],
+  providers: [CoreModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
